dc569b90596df510388961ade0399866231ab5a1
peterjohnhunt <git@peterjohnhunt.com>
2016-11-09 21:35:43 -0600

    Pieces

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,21 @@ jQuery(document).ready(function($){
 // _Section
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 
+$time     = $(".time"),
+$underlay = $(".underlay");
 
+$time.on('update',function(){
+	$time.text(moment().format('h:mm:ss a'));
+});
+
+$time.trigger('update');
+
+setInterval(function(){
+	$time.trigger('update');
+}, 1000);
+
+$underlay.click(function(){
+	$(this).toggleClass('open');
+});
 
 });
